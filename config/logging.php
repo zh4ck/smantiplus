68af0d47a0240644238fcc4d3741158d721c96d8
Zh4ck <zhacxk@gmail.com>
2024-07-25 00:22:37 +0700

    fa

--- config/logging.php
+++ config/logging.php
@@ -57,7 +57,7 @@ return [
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'level' => 'emergency',
         ],
 
         'daily' => [
